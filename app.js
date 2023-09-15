49442e7dacca7b9f67b3f9958e4d111006b6ed12
tgomezl <82736073+tgomezl@users.noreply.github.com>
2023-09-15 18:02:34 -0300

    cors agregado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app = express();
-
+const cors=require("cors")
+app.use(cors())
 const userRouter =require("./routes/userRouter")
 const reviewRouter= require("./routes/reviewRouter")
 const fixerRouter= require("./routes/fixerRouter")
